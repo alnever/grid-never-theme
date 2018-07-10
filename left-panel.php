e21e2c2a51a24e2d6f612a3d3b0184ad0574de87
Alex Neverov <al_neverov@live.ru>
2018-07-10 02:55:02 -0700

    Responsive CSS fixed

--- left-panel.php
+++ left-panel.php
@@ -3,4 +3,11 @@
 		<div class="main-menu">
 			<?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
 		</div>
+		
+        <div class="mobile-main-menu">
+            <span class="mobile-menu-start">
+                &equiv;&nbsp;<?php _e("Menu",'grid-never-theme'); ?>
+            </span>
+            <?php wp_nav_menu( array( 'theme_location' => 'header-menu' ) ); ?>
+        </div>
 	</div>
